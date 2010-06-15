9d4be138235e340efaec7156ec391b63ab974d11
Charlie DeTar <cfd@media.mit.edu>
2010-06-15 14:01:04 -0400

    more working...

--- laws/urls.py
+++ laws/urls.py
@@ -1,6 +1,6 @@
 from django.conf.urls.defaults import *
 
 urlpatterns = patterns('',
-    url('^(?P<title>\d+)/(?P<section>\d+)/(?P<psection>.+)$', 
+    url('^(?P<title>\d+)/(?P<section>\d+)/(?P<psection>.+)?$', 
         'laws.views.show_law', name='laws_show_law')
 )
