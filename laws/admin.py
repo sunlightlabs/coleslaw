dbc9b9b1bac2a05f8561be56ff15d528426a4740
Michael Stephens <me@mikej.st>
2010-06-16 11:00:42 -0400

    make references readonly in admin (too slow to low otherwise)

--- laws/admin.py
+++ laws/admin.py
@@ -4,5 +4,6 @@ from laws.models import Law
 class LawAdmin(admin.ModelAdmin):
     list_filter = ["title"]
     list_display = ["title", "section", "psection", "num_references", "level", "order", "text", "source"]
+    readonly_fields = ('references',)
 
 admin.site.register(Law, LawAdmin)
